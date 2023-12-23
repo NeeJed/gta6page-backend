10f691f61187190f9bac188b07327f28aaec3c59
NeeJed <belugin-denis2@rambler.ru>
2023-12-23 19:52:12 +0300

    Финальная версия по видосу того чела, но это такой кал, что буду переделывать, у чела 50к подписоты, хз как можно с такой аудиторией такую шляпу выкладывать

--- index.ts
+++ index.ts
@@ -1,18 +1,29 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors';
 
 const prisma = new PrismaClient()
 const app = express()
 const PORT = 5000;
 
+app.use(cors())
 app.use(express.json())
 
 app.post('/api', async (req, res) => {
     const {email, name} = req.body
-    if (!email || !name)
-        return res.status(400).send('Email and name required fields')
-    
-        res.json({message: 'Hello world!'})
+    if (!email || !name) {
+        return res.status(400).json({message: 'Email and name required fields'})
+    }
+    try {
+        const newRecord = await prisma.waitList.create({
+            data: {
+                email, name
+            },
+        })
+        res.json(newRecord)
+    } catch (error) {
+        res.status(400).json({message: error})
+    }
 })
 
 const server = app.listen(PORT, () => {
